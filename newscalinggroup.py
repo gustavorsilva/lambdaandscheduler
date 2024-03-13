8c338e84ac96d3370ed66a58749d5f1e0cb4d896
Gustavo Rodrigues <gustavo_rodriguees@hotmail.com>
2024-03-13 13:39:15 -0300

    alteracao de scaling em group atraves da action

--- newscalinggroup.py
+++ newscalinggroup.py
@@ -1,53 +1,63 @@
 import boto3
 import fnmatch
-import sys
 import os
 
 def perform_scaling_action(pattern, desired, min_size, max_size):
-    # Inicializar o cliente do serviço Auto Scaling
     autoscaling_client = boto3.client('autoscaling')
 
-    # Filtrar os grupos de Auto Scaling com base no padrão especificado
+    # Filtrar os grupos de Auto Scaling
     grupos_correspondentes = fnmatch.filter(get_all_auto_scaling_groups(), pattern)
 
     # Realizar a ação (start ou stop) para os grupos de Auto Scaling correspondentes
-    if action == 'start' or action == 'stop':
-        for grupo_correspondente in grupos_correspondentes:
-            try:
-                response = autoscaling_client.update_auto_scaling_group(
-                    AutoScalingGroupName=grupo_correspondente,
-                    DesiredCapacity=desired,
-                    MinSize=min_size,
-                    MaxSize=max_size
-                )
-                print(f"Grupo {grupo_correspondente} atualizado com sucesso.")
-                print(f"Novo DesiredCapacity: {desired}")
-                print(f"Novo MinSize: {min_size}")
-                print(f"Novo MaxSize: {max_size}")
-            except Exception as e:
-                print(f"Erro ao atualizar o grupo {grupo_correspondente}: {str(e)}")
+    for grupo_correspondente in grupos_correspondentes:
+        try:
+            response = autoscaling_client.update_auto_scaling_group(
+                AutoScalingGroupName=grupo_correspondente,
+                DesiredCapacity=desired,
+                MinSize=min_size,
+                MaxSize=max_size
+            )
+            print(f"Grupo {grupo_correspondente} atualizado com sucesso.")
+            print(f"Novo DesiredCapacity: {desired}")
+            print(f"Novo MinSize: {min_size}")
+            print(f"Novo MaxSize: {max_size}")
+        except Exception as e:
+            print(f"Erro ao atualizar o grupo {grupo_correspondente}: {str(e)}")
 
 def get_all_auto_scaling_groups():
-    # Inicializar o cliente do serviço Auto Scaling
     autoscaling_client = boto3.client('autoscaling')
 
-    # Obter a lista de todos os grupos de Auto Scaling
+    # lista de todos os grupos de Auto Scaling
     response = autoscaling_client.describe_auto_scaling_groups()
     grupos_auto_scaling = response['AutoScalingGroups']
     return [grupo['AutoScalingGroupName'] for grupo in grupos_auto_scaling]
 
-# Exemplo de uso do script Python
-if __name__ == "__main__":
-
-    action = os.environ.get('ACTION')
+def lambda_handler(event, context):
+    action = event.get('action')
+    desired = event.get('desired', 0)
+    min_size = event.get('min_size', 0)
+    max_size = event.get('max_size', 0)
 
     if action not in ['start', 'stop']:
-        sys.exit(1)
+        return {
+            'statusCode': 400,
+            'body': 'Ação inválida. As opções válidas são "start" e "stop".'
+        }
 
+    # start ou stop via action
     if action == 'start':
-        perform_scaling_action('testela*', 1, 1, 2)
+        perform_scaling_action('eks-node*', 1, 1, 1)
     elif action == 'stop':
-        perform_scaling_action('testelam*', 0, 0, 0)
-    
-    # Retornar uma mensagem de conclusão
-    print(f"Alterações de scaling realizadas com sucesso para a ação '{action}'")
+        perform_scaling_action('eks-node*', 0, 0, 0)
+
+    # Status de Atividade
+    return {
+        'statusCode': 200,
+        'body': f'Alterações de scaling realizadas com sucesso para a ação "{action}".'
+    }
+
+
+###### teste json ######
+#{
+#  "action": "start" ou "action": "stop"
+#}
