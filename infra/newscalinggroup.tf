10fcb7fa16d93eee8a6310d6c848402e45b00116
Gustavo Rodrigues <gustavo_rodriguees@hotmail.com>
2025-11-26 21:18:13 -0300

    Update newscalinggroup.tf

--- infra/newscalinggroup.tf
+++ infra/newscalinggroup.tf
@@ -1,63 +1,42 @@
-import boto3
-import fnmatch
-import os
+#########
+# LAMBDA
+#########
 
-def perform_scaling_action(pattern, desired, min_size, max_size):
-    autoscaling_client = boto3.client('autoscaling')
+resource "aws_lambda_function" "lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "stopec2"
+  role          = "arn:aws:iam::119739005737:role/startandstop"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+  publish       = true
+  timeout       = 30
+  memory_size   = 128
+}
 
-    # Filtrar os grupos de Auto Scaling
-    grupos_correspondentes = fnmatch.filter(get_all_auto_scaling_groups(), pattern)
+###########
+# scheduler
+###########
 
-    # Realizar a ação (start ou stop) para os grupos de Auto Scaling correspondentes
-    for grupo_correspondente in grupos_correspondentes:
-        try:
-            response = autoscaling_client.update_auto_scaling_group(
-                AutoScalingGroupName=grupo_correspondente,
-                DesiredCapacity=desired,
-                MinSize=min_size,
-                MaxSize=max_size
-            )
-            print(f"Grupo {grupo_correspondente} atualizado com sucesso.")
-            print(f"Novo DesiredCapacity: {desired}")
-            print(f"Novo MinSize: {min_size}")
-            print(f"Novo MaxSize: {max_size}")
-        except Exception as e:
-            print(f"Erro ao atualizar o grupo {grupo_correspondente}: {str(e)}")
+resource "aws_cloudwatch_event_rule" "profile_generator_stop_ec2" {
+  name = "profile-generator-stop-event-rule"
+  description = "Stop"
+  schedule_expression = "cron(15 07 ? * * *)"
+}
 
-def get_all_auto_scaling_groups():
-    autoscaling_client = boto3.client('autoscaling')
+resource "aws_cloudwatch_event_target" "profile_generator_stop_target" {
+  arn = "arn:aws:lambda:us-east-1:119739005737:function:stopec2"
+  rule = aws_cloudwatch_event_rule.profile_generator_stop_ec2.name
+  input = <<JSON
+{
+  "env": "dev",
+  "action": "Stop"
+} 
+  JSON
+}
 
-    # lista de todos os grupos de Auto Scaling
-    response = autoscaling_client.describe_auto_scaling_groups()
-    grupos_auto_scaling = response['AutoScalingGroups']
-    return [grupo['AutoScalingGroupName'] for grupo in grupos_auto_scaling]
-
-def lambda_handler(event, context):
-    action = event.get('action')
-    desired = event.get('desired', 0)
-    min_size = event.get('min_size', 0)
-    max_size = event.get('max_size', 0)
-
-    if action not in ['start', 'stop']:
-        return {
-            'statusCode': 400,
-            'body': 'Ação inválida. As opções válidas são "start" e "stop".'
-        }
-
-    # start ou stop via action
-    if action == 'start':
-        perform_scaling_action('eks-node*', 1, 1, 1)
-    elif action == 'stop':
-        perform_scaling_action('eks-node*', 0, 0, 0)
-
-    # Status de Atividade
-    return {
-        'statusCode': 200,
-        'body': f'Alterações de scaling realizadas com sucesso para a ação "{action}".'
-    }
-
-
-###### teste json ######
-#{
-#  "action": "start" ou "action": "stop"
-#}
+resource "aws_lambda_permission" "trigger_scheduller" {
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.lambda.function_name
+    principal = "events.amazonaws.com"
+    source_arn = "${aws_cloudwatch_event_rule.profile_generator_stop_ec2.arn}"
+}
