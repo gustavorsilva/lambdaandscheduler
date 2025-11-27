5df919d542223ce46fae188efb97f21fd5769ccd
Gustavo Rodrigues <gustavo_rodriguees@hotmail.com>
2025-11-27 00:13:36 -0300

    Update newscalinggroup.tf

--- infra/newscalinggroup.tf
+++ infra/newscalinggroup.tf
@@ -1,6 +1,6 @@
 module "exemplo_modulo" {
   # O segredo é adicionar ?ref=v3.0.0 ao final da URL
-  source = "git::https://github.com/sua-organizacao/seu-repo.git?ref=v3.0.0"
+  source = "git::https://github.com/sua-organizacao/seu-repo.git?ref=v4.0.0"
 
   # Variáveis do módulo (inputs)
   ambiente      = "producao"
